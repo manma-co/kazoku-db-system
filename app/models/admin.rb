a75c02952616de91640e1e3fcaa9df53abbce242
yoshi522 <yoshihito522@gmail.com>
2017-04-04 23:46:20 +0900

    Added omni auth controller and admin controller

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -3,5 +3,20 @@ class Admin < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   # devise :database_authenticatable, :registerable,
   #        :recoverable, :rememberable, :trackable, :validatable
-  devise :omniauthable, :omniauth_providers => [:google_oauth2]
+  devise :omniauthable, :trackable, :omniauth_providers => [:google_oauth2]
+
+  protected
+  def self.from_omniauth(access_token)
+    data = access_token.info
+    user = User.where(:email => data["email"]).first
+
+    # Uncomment the section below if you want users to be created if they don't exist
+    # unless user
+    #     user = User.create(name: data["name"],
+    #        email: data["email"],
+    #        password: Devise.friendly_token[0,20]
+    #     )
+    # end
+    user
+  end
 end
