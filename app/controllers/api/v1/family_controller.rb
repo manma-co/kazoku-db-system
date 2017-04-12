bc649379a20bf5c7b95401b92ac4dcf168bd95b2
yoshi522 <yoshihito522@gmail.com>
2017-04-13 00:42:50 +0900

    Enhance readability.

--- app/controllers/api/v1/family_controller.rb
+++ app/controllers/api/v1/family_controller.rb
@@ -1,22 +1,28 @@
 class Api::V1::FamilyController < Api::V1Controller
+
+  # To create new user data.
   def create
 
     # Header authorization key
     key = request.authorization
+
+    # Auth validation
     return json_response 'Invalid auth key length.', :unauthorized unless key.length == 32
     return json_response 'Invalid auth key. Make sure to use correct key.', :unauthorized if key != Rails.application.secrets.auth_key_base
 
     # Body is inside of request.request_parameters
     body = request.request_parameters
-    email = body['email_pc']
-    sex = body['sex']
-    name = body['name']
-    is_family = body['is_family']
-    job_style = body['job_style']
-    birthday_mother = body['birthday_mother']
-    birthday_father = body['birthday_father']
-    is_photo_ok = body['is_photo_ok']
-    is_sns_ok = body['is_sns_ok']
+
+    # Set parameter
+    email             = body['email_pc']
+    sex               = body['sex']
+    name              = body['name']
+    is_family         = body['is_family']
+    job_style         = body['job_style']
+    birthday_mother   = body['birthday_mother']
+    birthday_father   = body['birthday_father']
+    is_photo_ok       = body['is_photo_ok']
+    is_sns_ok         = body['is_sns_ok']
 
     # Validation
     return json_response 'Invalid email.',                  :bad_request if email == nil
@@ -37,26 +43,26 @@ class Api::V1::FamilyController < Api::V1Controller
     )
 
     profile_family = user.build_profile_family(
-        job_style: job_style,
-        number_of_children: body['number_of_children'],
-        is_photo_ok: is_photo_ok,
-        is_sns_ok: is_sns_ok
+        job_style:            job_style,
+        number_of_children:   body['number_of_children'],
+        is_photo_ok:          is_photo_ok,
+        is_sns_ok:            is_sns_ok
     )
 
     profile_mother = profile_family.profile_individuals.build(
         birthday:     birthday_mother,
-        job_domain: body['job_domain_mother'],
-        role: 'mother'
+        job_domain:   body['job_domain_mother'],
+        role:         'mother'
     )
     profile_father = profile_family.profile_individuals.build(
         birthday:     birthday_father,
-        job_domain: body['job_domain_father'],
-        role: 'father'
+        job_domain:   body['job_domain_father'],
+        role:         'father'
     )
 
     contact = user.build_contact(
-        email_pc: email,
-        email_phone: body['email_phone'],
+        email_pc:     email,
+        email_phone:  body['email_phone'],
     )
 
     location = user.build_location(
@@ -67,12 +73,12 @@ class Api::V1::FamilyController < Api::V1Controller
     if user.save && profile_family.save && profile_mother.save &&
         profile_father.save && contact.save && location.save
       data = {
-          user: user,
-          location: location,
-          contact: contact,
-          family: profile_family,
-          mother: profile_mother,
-          father: profile_father
+          user:       user,
+          location:   location,
+          contact:    contact,
+          family:     profile_family,
+          mother:     profile_mother,
+          father:     profile_father
       }
       json_response data, :created
     else
