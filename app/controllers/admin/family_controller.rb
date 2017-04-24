a48b51697873f73c075621dc7a382d8b2a987e55
Ryutaro <ricken0203@gmail.com>
2017-04-24 11:38:22 +0900

    Add family-link from list-page

--- app/controllers/admin/family_controller.rb
+++ app/controllers/admin/family_controller.rb
@@ -3,9 +3,7 @@ class Admin::FamilyController < Admin::AdminController
   end
 
   def show
-    p user_id = params[:id] # user no id
-
-    p user = User.find(user_id)
-
+    user_id = params[:id]
+    @family = User.find(user_id)
   end
 end
