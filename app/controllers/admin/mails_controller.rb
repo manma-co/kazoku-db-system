e16bc5302499d909a3602dd93d2dfcd72e49c67d
shino <harimanmon@gmail.com>
2017-04-27 00:32:36 +0900

    [Update] 打診メール作成画面作成

--- app/controllers/admin/mails_controller.rb
+++ app/controllers/admin/mails_controller.rb
@@ -1,7 +1,8 @@
 class Admin::MailsController < Admin::AdminController
 
   def new
-
+    user_index = params[:user_id]
+    @users = User.where(id: user_index).includes(:profile_family, :location, :contact)
   end
 
   def confirm
