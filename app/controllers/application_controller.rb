a03b8f6ff9396a0f531f97becc165ec131b5f6b6
shino <harimanmon@gmail.com>
2017-04-30 03:46:58 +0900

    [Update] ログイン後のリダイレクト先の変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  # ログイン後のリダイレクト先の変更
+  def after_sign_in_path_for(resource)
+    admin_family_index_path
+  end
+
 end
