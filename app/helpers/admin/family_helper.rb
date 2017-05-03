c45034cdc1624457916b23d9a9aa459d24b38dce
shino <harimanmon@gmail.com>
2017-05-03 17:43:44 +0900

    [Update] is_sns_ok を is_report_okに変更。第一子の生年月日と子どもの数の表示

--- app/helpers/admin/family_helper.rb
+++ app/helpers/admin/family_helper.rb
@@ -1,6 +1,8 @@
 module Admin::FamilyHelper
 
   # 働き方の文字列を取得する
+  # @param [int] job_style 働き方ステータス
+  # @return [String] 働き方文字列
   def job_style_str(job_style)
     if job_style == Settings.job_style.both
       Settings.job_style.str.both
@@ -12,6 +14,8 @@ module Admin::FamilyHelper
   end
 
   # 男性OK or NG文字列を取得する
+  # @param [bool] is_male_ok 男性の参加者を許可するか
+  # @return [String] OK or NG
   def is_male_ok_str(is_male_ok)
     if is_male_ok == Settings.is_male.ok
       'OK'
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  # 日付をフォーマットする
+  # @param [date] date Date型の情報
+  # @return [String] フォーマットされた日付データ
+  def formatted_date(date)
+    date&.strftime('%Y年%m月%d日')
+  end
 end
