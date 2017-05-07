a1893b13ce0506278251a796ca3ce2f46b8ebfe8
yoshi522 <yoshihito522@gmail.com>
2017-05-07 11:31:12 +0900

    Added send email function to families

--- app/mailers/common_mailer.rb
+++ app/mailers/common_mailer.rb
@@ -2,11 +2,22 @@ class CommonMailer < ActionMailer::Base
   default from: 'Manma <info@manma.co>'
   layout 'mailer'
 
+  add_template_helper(TextHelper)
 
   def matched_email(contact)
     @contact = contact
     @user = @contact.user
     mail(to: @contact.email_pc, subject: '【重要 / 家族留学】マッチングしました')
+  end
+
+  def matched_email_to_family(title, body, users)
+    @users = users
+    @body = body
+    mails = ''
+    @users.each do |user|
+      mails += user.contact.email_pc + ', '
+    end
+    mail(to: mails, subject: title)
   end
 
 end
