fe5cfbf704e4eb6ff02c7e20785c40e093b62a9d
Ryutaro <ricken0203@gmail.com>
2017-05-07 16:16:10 +0900

    [add] モデルの属性を追加(とりあえずstring型で定義)

--- db/migrate/20170412133549_create_profile_families.rb
+++ db/migrate/20170412133549_create_profile_families.rb
@@ -3,10 +3,16 @@ class CreateProfileFamilies < ActiveRecord::Migration[5.0]
     create_table :profile_families do |t|
       t.integer :job_style # 1 means both work, 0 means mother or father work, another does not.
       t.integer :number_of_children
-      t.integer :is_photo_ok  # 1 means ok, 2 means ok if the face of child is not clear, 0 means bad.
-      t.integer :is_report_ok  # 1 means ok, 2 means ok if the face of child is not clear, 0 means bad.
+      t.integer :is_photo_ok # 1 means ok, 2 means ok if the face of child is not clear, 0 means bad.
+      t.integer :is_report_ok # 1 means ok, 2 means ok if the face of child is not clear, 0 means bad.
       t.integer :is_male_ok # 1 means ok, 0 means NG
+      t.string :has_time_shortening_experience
+      t.string :has_childcare_leave_experience
+      t.string :has_job_change_experience
+      t.string :marriaged_mother_age
+      t.string :birthed_mother_age
       t.date :child_birthday
+      t.string :opinion_or_question
 
       t.timestamps
     end
