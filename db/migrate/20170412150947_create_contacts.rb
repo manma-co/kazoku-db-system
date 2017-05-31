718c172a12968d39cd0a8948e8383973e1f9e034
Ryutaro <ricken0203@gmail.com>
2017-05-31 23:27:33 +0900

    [Add] phone_number attribute

--- db/migrate/20170412150947_create_contacts.rb
+++ db/migrate/20170412150947_create_contacts.rb
@@ -3,6 +3,7 @@ class CreateContacts < ActiveRecord::Migration[5.0]
     create_table :contacts do |t|
       t.string :email_pc
       t.string :email_phone
+      t.string :phone_number
 
       t.timestamps
     end
