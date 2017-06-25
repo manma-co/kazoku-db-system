b996f2249d482002bfebb02455877a5b3a9e10eb
Ryutaro <ricken0203@gmail.com>
2017-06-25 21:56:22 +0900

    schema.rbのアップデート

--- db/schema.rb
+++ db/schema.rb
@@ -27,8 +27,9 @@ ActiveRecord::Schema.define(version: 20170604011839) do
   create_table "contacts", force: :cascade do |t|
     t.string   "email_pc"
     t.string   "email_phone"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.string   "phone_number"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
     t.integer  "user_id"
     t.index ["user_id"], name: "index_contacts_on_user_id"
   end
