de90a408cd4c76a65652f3ecf9a03dedf00d9bea
shino <harimanmon@gmail.com>
2017-07-02 03:27:19 +0900

    [Modify] requestに適用しているcssがadminに適用されていたので修正

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,5 +10,6 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 Rails.application.config.assets.precompile += %w( request/request.css )
+Rails.application.config.assets.precompile += %w( admin/admin.css )
 Rails.application.config.assets.precompile += %w( request.js )
 Rails.application.config.assets.precompile += %w( custom.css )
