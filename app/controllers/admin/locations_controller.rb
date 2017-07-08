259faf227c292b93d1c78a2e2b80ff3005829593
shino <harimanmon@gmail.com>
2017-07-08 13:57:11 +0900

    [Update] #135 エラー発生時の対策

--- app/controllers/admin/locations_controller.rb
+++ app/controllers/admin/locations_controller.rb
@@ -18,11 +18,21 @@ class Admin::LocationsController < Admin::AdminController
   def search
     @address = params[:address]
     family_list = family_params
-    if params[:address].present?
-      geocoder = Geocoder.search(params[:address])
-      @search_result_address = geocoder[0].formatted_address
-      location = geocoder[0].geometry['location']
-      @candidate_hash = Location.candidate_list(location, family_list)
+    begin
+      if params[:address].present?
+        geocoder = Geocoder.search(params[:address])
+        @search_result_address = geocoder[0].formatted_address
+        location = geocoder[0].geometry['location']
+        @candidate_hash = Location.candidate_list(location, family_list)
+      end
+    rescue
+      # Google API error: over query limit. 対策
+      @search_result_address = 'エラーが発生しました。全件表示します。'
+      @candidate_hash = {}
+      locations = family_list.map { |f| f.user.location }
+      locations.each do |l|
+        @candidate_hash.store(l, 0)
+      end
     end
   end
 
@@ -33,7 +43,7 @@ class Admin::LocationsController < Admin::AdminController
     @job_style = params[:job_style] || Settings.job_style.none
     @job_style = @job_style.to_i
     if @job_style == Settings.job_style.both
-      job_style = [Settings.job_style.both, Settings.job_style.both_single]  # シングルも含む
+      job_style = [Settings.job_style.both, Settings.job_style.both_single] # シングルも含む
       family = ProfileFamily.where(job_style: job_style)
     elsif @job_style == Settings.job_style.homemaker
       job_style = Settings.job_style.homemaker
