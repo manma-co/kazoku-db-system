e45a84a71484f482b77b0e548667cf34ed09d679
yoshi522 <yoshihito522@gmail.com>
2017-07-17 12:45:36 +0900

    Added mail template.

--- app/helpers/text_helper.rb
+++ app/helpers/text_helper.rb
@@ -3,4 +3,9 @@ module TextHelper
     target = html_escape(target)
     target.gsub(/\r\n|\r|\n/, "<br>")
   end
+
+  def markdown(text)
+    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true, tables: true)
+    markdown.render(text).html_safe
+  end
 end
--- app/mailers/news_letter_mailer.rb
+++ app/mailers/news_letter_mailer.rb
@@ -1,9 +1,11 @@
 class NewsLetterMailer < ApplicationMailer
 
+  add_template_helper(TextHelper)
+
   # ニュースレターを送信する
   def send_news_letter(news_letter, bcc_address)
-    @body = news_letter.body
-    subject = @news_letter.subject
+    @body = news_letter.content
+    subject = news_letter.subject
     mail(to: 'info@manma.co', bcc: bcc_address, subject: subject)
 
     # Insert to DB
@@ -13,7 +15,7 @@ class NewsLetterMailer < ApplicationMailer
         :bcc_address => bcc_address,
         :subject => subject,
         :body_text => '',
-        :request_log => 0,
+        :request_log => RequestLog.first,
         :retry_count => 0,
         :sent_status => true,
         :email_type => 'send_news_letter',
