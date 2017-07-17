96bf829ed89795ff7049d2e4e573f5aaf2126780
yoshi522 <yoshihito522@gmail.com>
2017-07-17 11:42:52 +0900

    Created news saving function.

--- app/controllers/admin/news_letter_controller.rb
+++ app/controllers/admin/news_letter_controller.rb
@@ -11,12 +11,38 @@ class Admin::NewsLetterController < Admin::AdminController
   end
 
   def create
+    @news_letter = NewsLetter.new(news_letter_params)
+    set_right_time
+    if @news_letter.save
+      redirect_to admin_news_letter_index_path
+    end
   end
 
   def edit
   end
 
   def update
+  end
+
+
+  private
+
+  def set_right_time
+    if @news_letter.distribution.present?
+      Time.zone = "Asia/Tokyo"
+      @news_letter.distribution = Time.zone.parse(news_letter_params[:distribution]).utc
+    end
+  end
+
+  def news_letter_params
+    params.require(:news_letter).permit(
+        :subject,
+        :distribution,
+        :send_to,
+        :content,
+        :sent_at,
+        :is_save
+    )
   end
 
 end
