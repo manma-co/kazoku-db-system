ab9dde4b8e32787b95875da41e7593cc61c177d0
yoshi522 <yoshihito522@gmail.com>
2017-08-31 21:02:27 +0900

    [enhance.#201] admin/family: Added request history

--- app/controllers/admin/family_controller.rb
+++ app/controllers/admin/family_controller.rb
@@ -4,9 +4,13 @@ class Admin::FamilyController < Admin::AdminController
   end
 
   def show
-    user_id = params[:id]
-    @family = User.find(user_id)
-    @mother = @family.profile_family&.profile_individuals&.find_by(role: 'mother')
-    @father = @family.profile_family&.profile_individuals&.find_by(role: 'father')
+    user_id   = params[:id]
+    @family   = User.find(user_id)
+    @mother   = @family.profile_family&.profile_individuals&.find_by(role: 'mother')
+    @father   = @family.profile_family&.profile_individuals&.find_by(role: 'father')
+    @requests = EmailQueue.where(
+        email_type: Settings.email_type.request,
+        to_address: @family.contact.email_pc
+    )
   end
 end
