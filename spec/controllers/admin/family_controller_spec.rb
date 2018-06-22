09f41b4d727a3eea3aed02fb7d02f57732ff4e41
shino <harimanmon@gmail.com>
2018-06-23 03:47:05 +0900

    feat(spec): add spec

--- spec/controllers/admin/family_controller_spec.rb
+++ spec/controllers/admin/family_controller_spec.rb
@@ -2,10 +2,12 @@ require 'rails_helper'
 
 RSpec.describe Admin::FamilyController, type: :controller do
   describe 'GET #index' do
+    let(:users) { FactoryBot.create_list :user, 2 }
     before { login_admin }
     context 'index' do
       before { get :index }
       it { expect(response).to have_http_status(:ok) }
+      it { expect(assigns :users).to eq users }
     end
   end
 end
