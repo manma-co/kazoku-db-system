1ab7f71da03c8b0df2bc121efebb2787599d7ca4
shino <harimanmon@gmail.com>
2018-06-24 12:33:56 +0900

    feat(spec): Add test admin is not loggedin feat(spec): Add test destroy user

--- spec/controllers/admin/family_controller_spec.rb
+++ spec/controllers/admin/family_controller_spec.rb
@@ -3,12 +3,18 @@ require 'rails_helper'
 RSpec.describe Admin::FamilyController, type: :controller do
   describe 'GET #index' do
     let(:users) { FactoryBot.create_list :user, 2 }
-    before { login_admin }
     context 'index' do
-      before { get :index }
+      before {
+        login_admin
+        get :index
+      }
       it { expect(response).to have_http_status(:ok) }
       it { expect(response).to render_template :index }
       it { expect(assigns(:users)).to eq users }
+    end
+    context 'index when admin is not logged in' do
+      it { expect(response).to have_http_status(:ok) }
+      it { expect(response).to render_template nil }
     end
   end
 
@@ -29,6 +35,16 @@ RSpec.describe Admin::FamilyController, type: :controller do
       it { expect(assigns(:requests)).to eq [] }
       it { expect(assigns(:mother)).to eq mother }
       it { expect(assigns(:father)).to eq father }
+    end
+  end
+
+  describe 'DELETE #destroy' do
+    let(:user) { FactoryBot.create(:user) }
+    before { login_admin }
+    context 'destroy' do
+      before { delete :destroy, params: { id: user.id } }
+      it { expect(response).to have_http_status(:redirect) }
+      it { expect(response).to redirect_to admin_family_index_path }
     end
   end
 end
