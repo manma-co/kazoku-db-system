69c669c177bca894a2ae243ea3e4db2496456eb6
shino <harimanmon@gmail.com>
2018-06-24 14:31:49 +0900

    fix: ブラウザバック時に日付情報が消えてしまうバグの修正
    セッション上に消えない日付情報があるので、その情報をパースして利用する実装に修正

--- app/controllers/admin/mails_controller.rb
+++ app/controllers/admin/mails_controller.rb
@@ -69,12 +69,12 @@ class Admin::MailsController < Admin::AdminController
 
     # 希望日程、開始日時、終了日時をパースして文字列化 -> 配列
     ((0..4).to_a.map { |i|
-      date_key = "date#{i}_submit".to_sym
+      date_key = "date#{i}".to_sym
       start_time_key = "start_time#{i}".to_sym
       finish_time_key = "finish_time#{i}".to_sym
 
-      date = params[date_key]
-      next if date == ''
+      next if params[date_key].empty?
+      date = parse_date(params[date_key])
       start_time = params[start_time_key]
       finish_time = params[finish_time_key]
 
@@ -82,46 +82,56 @@ class Admin::MailsController < Admin::AdminController
     }).join(br)
   end
 
+  # 「2018年06月20日 土曜日」という文字列を2018/06/20 に変換する
+  def parse_date(date_str)
+    return nil if date_str.empty?
+    date = date_str.split(' ')[0]
+    date.sub!('年', '/')
+    date.sub!('月', '/')
+    date.sub!('日', '')
+    date
+  end
+
   # 本文のテンプレートで初期化する
   def init_mail
     @title =  '【要確認】家族留学受け入れのお願い'
     @body = <<-EOS
- 
+
 
 こんにちは、manma マッチング担当です。
 いつも大変お世話になっております。
- 
+
 家族留学を受け入れて頂きたく、ご連絡致しました。
-[manma_user_name]さまのお宅への家族留学を希望されている方がいらっしゃいます。 
+[manma_user_name]さまのお宅への家族留学を希望されている方がいらっしゃいます。
 
 打診内容をご確認いただき、受け入れ可能な日程がございましたら
 下記URLよりご回答をお願いいたします。
 
- 
+
 ○  参加者プロフィール
 氏名：[manma_template_student_name]
 所属：[manma_template_student_belongs_to]
 最寄り駅：[manma_template_station]
 参加動機：[manma_template_motivation]
- 
+
 【候補日】
 [manma_template_dates]
 
 ＜この中の一部の時間での受け入れで構いません。ご協力のほどどうぞよろしくお願いいたします。＞
 
- 
+
 【回答用URL】
 [manma_request_link]
 
- 
+
 ＊  注意事項＊
 ・  この受け入れのお願いは、複数のご家庭にお送りさせていただいております。
 ・  最初に受け入れ可能とご連絡いたただいたご家庭が発生した時点で、マッチング成立となり、このURLは無効となりますので、予めご了承くださいませ。
- 
+
 
 何かご不明な点がございましたら、info@manma.co(担当：久保)までご連絡ください。
 
- 
+
 manma
 EOS
   end
---
+++ spec/controllers/admin/mails_controller_spec.rb
@@ -0,0 +1,66 @@
+require 'rails_helper'
+
+RSpec.describe Admin::MailsController, type: :controller do
+  describe 'GET #new' do
+    let(:users) { FactoryBot.create_list :user, 10 }
+
+    before do
+      login_admin
+      get :new, params: { user_id: users.pluck(:id) }
+    end
+    context 'new' do
+      it { expect(response).to have_http_status(:ok) }
+      it { expect(response).to render_template :new }
+      it { expect(assigns(:users)).to eq users }
+      it { expect(assigns(:title)).to eq '【要確認】家族留学受け入れのお願い' }
+    end
+  end
+
+  describe 'POST #confirm' do
+    let(:users) { FactoryBot.create_list :user, 2 }
+    before do
+      login_admin
+      body = <<-EOS
+[manma_user_name]さまのお宅への家族留学を希望されている方がいらっしゃいます。
+
+○  参加者プロフィール
+氏名：[manma_template_student_name]
+所属：[manma_template_student_belongs_to]
+最寄り駅：[manma_template_station]
+参加動機：[manma_template_motivation]
+
+【候補日】
+[manma_template_dates]
+
+【回答用URL】
+[manma_request_link]
+EOS
+      post :confirm, params: {
+        user_id: users.pluck(:id),
+        title: 'Sample Title',
+        email: 'test@example.com',
+        emergency: '09012345678',
+        student_name: '伊集院空太',
+        belongs_to: '慶應義塾大学',
+        station: '北千住駅',
+        motivation: '環境保護',
+        date0: '2018年06月23日 土曜日',
+        date1: '2018年06月24日 日曜日',
+        date2: '',
+        date3: '',
+        date4: '',
+        start_time0: '10:00',
+        start_time1: '11:00',
+        finish_time0: '20:00',
+        finish_time1: '21:00',
+        body: body,
+      }
+    end
+    context 'confirm' do
+      it { expect(response).to have_http_status(:ok) }
+      it { expect(response).to render_template :confirm }
+      it { expect(assigns(:body)).to include('2018/06/23 10:00 ~ 20:00') }
+      it { expect(assigns(:body)).to include('2018/06/24 11:00 ~ 21:00') }
+    end
+  end
+end
