c27ac867c90a88f0355f50ef361b848b7cec3360
harhogefoo <harimanmon@gmail.com>
2020-07-02 00:29:03 +0900

    :sparkles: #287

--- db/schema.rb
+++ db/schema.rb
@@ -10,180 +10,180 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200213163120) do
+ActiveRecord::Schema.define(version: 2020_02_13_163120) do
 
   create_table "admins", force: :cascade do |t|
-    t.string   "email",              default: "", null: false
-    t.integer  "sign_in_count",      default: 0,  null: false
+    t.string "email", default: "", null: false
+    t.integer "sign_in_count", default: 0, null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.datetime "created_at",                      null: false
-    t.datetime "updated_at",                      null: false
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
   end
 
   create_table "contacts", force: :cascade do |t|
-    t.string   "email_pc"
-    t.string   "email_phone"
-    t.string   "phone_number"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
-    t.integer  "user_id"
+    t.string "email_pc"
+    t.string "email_phone"
+    t.string "phone_number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
     t.index ["user_id"], name: "index_contacts_on_user_id"
   end
 
   create_table "email_queues", force: :cascade do |t|
-    t.string   "sender_address",  null: false
-    t.string   "to_address",      null: false
-    t.string   "cc_address"
-    t.string   "bcc_address"
-    t.string   "subject"
-    t.text     "body_text"
-    t.integer  "study_abroad_id"
-    t.integer  "retry_count"
-    t.boolean  "sent_status"
-    t.string   "email_type"
+    t.string "sender_address", null: false
+    t.string "to_address", null: false
+    t.string "cc_address"
+    t.string "bcc_address"
+    t.string "subject"
+    t.text "body_text"
+    t.integer "study_abroad_id"
+    t.integer "retry_count"
+    t.boolean "sent_status"
+    t.string "email_type"
     t.datetime "time_delivered"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["study_abroad_id"], name: "index_email_queues_on_study_abroad_id"
   end
 
   create_table "event_dates", force: :cascade do |t|
-    t.date     "hold_date"
-    t.time     "start_time"
-    t.time     "end_time"
-    t.integer  "user_id"
-    t.integer  "study_abroad_id"
-    t.string   "meeting_place"
-    t.string   "emergency_contact"
-    t.boolean  "is_first_time",     default: false
-    t.text     "information"
-    t.datetime "created_at",                        null: false
-    t.datetime "updated_at",                        null: false
-    t.boolean  "is_amazon_card"
+    t.date "hold_date"
+    t.time "start_time"
+    t.time "end_time"
+    t.integer "user_id"
+    t.integer "study_abroad_id"
+    t.string "meeting_place"
+    t.string "emergency_contact"
+    t.boolean "is_first_time", default: false
+    t.text "information"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.boolean "is_amazon_card"
     t.index ["study_abroad_id"], name: "index_event_dates_on_study_abroad_id"
     t.index ["user_id"], name: "index_event_dates_on_user_id"
   end
 
   create_table "job_domains", force: :cascade do |t|
-    t.string   "domain"
+    t.string "domain"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "locations", force: :cascade do |t|
-    t.string   "address"
-    t.decimal  "latitude",   precision: 10, scale: 6
-    t.decimal  "longitude",  precision: 10, scale: 6
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.integer  "user_id"
+    t.string "address"
+    t.decimal "latitude", precision: 10, scale: 6
+    t.decimal "longitude", precision: 10, scale: 6
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
     t.index ["user_id"], name: "index_locations_on_user_id"
   end
 
   create_table "news_letters", force: :cascade do |t|
-    t.string   "subject"
-    t.text     "content"
+    t.string "subject"
+    t.text "content"
     t.datetime "distribution"
-    t.string   "send_to"
-    t.boolean  "is_sent",      default: false
-    t.boolean  "is_save",      default: false
-    t.boolean  "is_monthly",   default: false
-    t.datetime "created_at",                   null: false
-    t.datetime "updated_at",                   null: false
+    t.string "send_to"
+    t.boolean "is_sent", default: false
+    t.boolean "is_save", default: false
+    t.boolean "is_monthly", default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "participants", force: :cascade do |t|
-    t.string   "name"
-    t.string   "kana"
-    t.string   "belong"
-    t.string   "email"
+    t.string "name"
+    t.string "kana"
+    t.string "belong"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_participants_on_email", unique: true
   end
 
   create_table "profile_families", force: :cascade do |t|
-    t.integer  "job_style"
-    t.integer  "number_of_children"
-    t.integer  "is_photo_ok"
-    t.integer  "is_report_ok"
-    t.integer  "is_male_ok"
-    t.string   "has_time_shortening_experience"
-    t.string   "has_childcare_leave_experience"
-    t.string   "has_job_change_experience"
-    t.string   "married_mother_age"
-    t.string   "married_father_age"
-    t.string   "first_childbirth_mother_age"
-    t.date     "child_birthday"
-    t.string   "opinion_or_question"
-    t.datetime "created_at",                     null: false
-    t.datetime "updated_at",                     null: false
-    t.integer  "user_id"
+    t.integer "job_style"
+    t.integer "number_of_children"
+    t.integer "is_photo_ok"
+    t.integer "is_report_ok"
+    t.integer "is_male_ok"
+    t.string "has_time_shortening_experience"
+    t.string "has_childcare_leave_experience"
+    t.string "has_job_change_experience"
+    t.string "married_mother_age"
+    t.string "married_father_age"
+    t.string "first_childbirth_mother_age"
+    t.date "child_birthday"
+    t.string "opinion_or_question"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
     t.index ["user_id"], name: "index_profile_families_on_user_id"
   end
 
   create_table "profile_individuals", force: :cascade do |t|
     t.datetime "birthday"
-    t.string   "hometown"
-    t.string   "role"
-    t.string   "company"
-    t.string   "career"
-    t.string   "has_experience_abroad"
-    t.datetime "created_at",            null: false
-    t.datetime "updated_at",            null: false
-    t.integer  "profile_family_id"
-    t.integer  "job_domain_id"
-    t.string   "job_domain_str"
+    t.string "hometown"
+    t.string "role"
+    t.string "company"
+    t.string "career"
+    t.string "has_experience_abroad"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "profile_family_id"
+    t.integer "job_domain_id"
+    t.string "job_domain_str"
     t.index ["job_domain_id"], name: "index_profile_individuals_on_job_domain_id"
     t.index ["profile_family_id"], name: "index_profile_individuals_on_profile_family_id"
   end
 
   create_table "request_days", force: :cascade do |t|
-    t.integer  "study_abroad_id"
-    t.date     "day"
-    t.string   "time"
-    t.boolean  "decided",         default: false
-    t.datetime "created_at",                      null: false
-    t.datetime "updated_at",                      null: false
+    t.integer "study_abroad_id"
+    t.date "day"
+    t.string "time"
+    t.boolean "decided", default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["study_abroad_id"], name: "index_request_days_on_study_abroad_id"
   end
 
   create_table "study_abroad_requests", force: :cascade do |t|
-    t.boolean  "result"
-    t.integer  "study_abroad_id"
-    t.integer  "user_id"
-    t.datetime "created_at",                  null: false
-    t.datetime "updated_at",                  null: false
-    t.integer  "answer_status",   default: 0
+    t.boolean "result"
+    t.integer "study_abroad_id"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "answer_status", default: 0
     t.index ["answer_status"], name: "index_study_abroad_requests_on_answer_status"
     t.index ["study_abroad_id"], name: "index_study_abroad_requests_on_study_abroad_id"
     t.index ["user_id"], name: "index_study_abroad_requests_on_user_id"
   end
 
   create_table "study_abroads", force: :cascade do |t|
-    t.string   "hashed_key"
-    t.string   "name"
-    t.string   "belongs"
-    t.string   "station"
-    t.string   "email"
-    t.string   "emergency"
-    t.text     "motivation"
+    t.string "hashed_key"
+    t.string "name"
+    t.string "belongs"
+    t.string "station"
+    t.string "email"
+    t.string "emergency"
+    t.text "motivation"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "name",                                   null: false
-    t.string   "kana",                                   null: false
-    t.integer  "gender",                                 null: false
-    t.boolean  "is_family",               default: true, null: false
-    t.string   "spread_sheets_timestamp"
-    t.datetime "created_at",                             null: false
-    t.datetime "updated_at",                             null: false
+    t.string "name", null: false
+    t.string "kana", null: false
+    t.integer "gender", null: false
+    t.boolean "is_family", default: true, null: false
+    t.string "spread_sheets_timestamp"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.datetime "deleted_at"
   end
 
