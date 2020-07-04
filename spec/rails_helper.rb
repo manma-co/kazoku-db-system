cd32f7a791a7065d23bcc750995a7cb629581c26
harhogefoo <harimanmon@gmail.com>
2020-07-04 09:54:10 +0900

    🔊 add log

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -60,6 +60,8 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 
+  config.include ActiveSupport::Testing::TimeHelpers
+
   # devise
   config.include Devise::Test::ControllerHelpers, type: :controller
   config.include Devise::Test::ControllerHelpers, type: :view
